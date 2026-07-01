321e7683a04c04d7c725867dd45818888bed23ae
Murza4 <75178526+Murza4@users.noreply.github.com>
2026-07-01 11:13:36 +0300

    Add analyzer facade

---
+++ src/bdr_wizard/analyzer/__init__.py
@@ -0,0 +1,5 @@
+from bdr_wizard.excel.compare import compare_workbooks
+
+from .service import WorkbookAnalysisError, WorkbookAnalyzer
+
+__all__ = ["WorkbookAnalysisError", "WorkbookAnalyzer", "compare_workbooks"]
