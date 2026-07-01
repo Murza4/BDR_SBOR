7c33dd8dd14ba10ec9845653d0e612c3e84de07e
Murza4 <75178526+Murza4@users.noreply.github.com>
2026-07-01 11:13:37 +0300

    Add analyzer facade service

---
+++ src/bdr_wizard/analyzer/service.py
@@ -0,0 +1,17 @@
+from bdr_wizard.excel.analyzer import (
+    WorkbookAnalysisCache,
+    WorkbookAnalysisError,
+    WorkbookAnalyzer,
+    build_workbook_meta,
+    normalize_formula,
+    normalize_text,
+)
+
+__all__ = [
+    "WorkbookAnalysisCache",
+    "WorkbookAnalysisError",
+    "WorkbookAnalyzer",
+    "build_workbook_meta",
+    "normalize_formula",
+    "normalize_text",
+]
